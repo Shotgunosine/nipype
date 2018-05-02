330e24c8de78b2916cb8ad2218325d50c17db462
Syed Hussain Ather <shussainather@gmail.com>
2018-05-02 12:33:02 -0400

    initial commit

---
+++ nipype/interfaces/meld/__init__.py
@@ -0,0 +1,12 @@
+#emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
+#ex: set sts=4 ts=4 sw=4 et:
+### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
+#
+#   See the COPYING file distributed along with the PTSA package for the
+#   copyright and license terms.
+#
+### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
+
+from . import cluster, meld, nonparam, stat_helper
+
+__all__ = ['cluster', 'meld', 'nonparam', 'stat_helper']
